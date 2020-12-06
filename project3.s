7ac892e63d269e4b850678067988fb775a8a203f
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-12-06 14:51:02 -0500

    First part of accepting user input

--- project3.s
+++ project3.s
@@ -1,4 +1,10 @@
 #My ID is 02935002 | (02935002 % 11) + 26 = 30
 .data
 	whoops: .asciiz "NaN"
-	userInput:  .word 4 #take in a word
+	userInput:  .space 1001 #take in a word
+
+.text
+	main:	
+		#gets user input
+		li $v0, 8 #get user input (text)
+		la $a0, userInput
